45237e93ef6521b4f3370fbfa5f8f029ac78b51a
Joshua Waugh <joshua@waugh.zone>
2019-02-12 16:11:01 +0200

    Added Swedish translations. Not fully implemented, but working.

--- app/src/main/java/com/example/hospitalguide/ActivityHospitalSearch.java
+++ app/src/main/java/com/example/hospitalguide/ActivityHospitalSearch.java
@@ -1,23 +1,29 @@
 package com.example.hospitalguide;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.DataSetObserver;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class ActivityHospitalSearch extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button search = findViewById(R.id.btnSearch);
-        search.setOnClickListener(new MyClick());
 
         Spinner treatment = findViewById(R.id.spTreatment);
         treatment.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, Hospitals.getInstance().getTreatments()));
@@ -26,18 +32,28 @@ public class ActivityHospitalSearch extends AppCompatActivity {
         Spinner locations = findViewById(R.id.spCities);
         locations.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.cities)));
         locations.setOnItemSelectedListener(new MyItemListener());
+
+        Spinner suburbs = findViewById(R.id.spRegions);
+        suburbs.setPrompt("- Select a suburb");
+        //suburbs.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, "- Select a suburb"));
+        suburbs.setOnItemSelectedListener(new MyItemListener());
+
+        Button search = findViewById(R.id.btnSearch);
+        search.setOnClickListener(new MyClick());
     }
 
     private class MyItemListener implements AdapterView.OnItemSelectedListener {
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-            Spinner suburbs = findViewById(R.id.spRegions);
 
+            //int selected = getResources().getIdentifier(parent.getSelectedItem().toString().toLowerCase(),"string", ActivityHospitalSearch.this.getPackageName());
+            Spinner suburbs = findViewById(R.id.spRegions);
+            //suburbs.setAdapter(new ArrayAdapter<>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, suburbGetter(pos)));
             switch(parent.getId()){
                 case R.id.spTreatment:
                     break;
                 case R.id.spCities:
-                    suburbs.setAdapter(new ArrayAdapter<>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, suburbGetter(parent.getItemAtPosition(pos).toString())));
+                    suburbs.setAdapter(new ArrayAdapter<>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, suburbGetter(pos)));
                     suburbs.setOnItemSelectedListener(new MyItemListener());
                     break;
                 case R.id.spRegions:
@@ -50,27 +66,47 @@ public class ActivityHospitalSearch extends AppCompatActivity {
 
     private class MyClick implements View.OnClickListener {
         @Override
-        public void onClick(View view){
+        public void onClick(View view) {
             Spinner treatment = findViewById(R.id.spTreatment);
             Spinner locations = findViewById(R.id.spCities);
             Spinner suburbs = findViewById(R.id.spRegions);
 
             Intent intent = new Intent(view.getContext(), ActivityHospitalList.class);
             intent.putExtra("treatment", treatment.getSelectedItem().toString());
-            intent.putExtra("city", locations.getSelectedItem().toString());
+            intent.putExtra("city", locations.getSelectedItem().toString().substring(0,1));
             intent.putExtra("region", suburbs.getSelectedItem().toString());
             startActivity(intent);
+
         }
     }
 
-    private String[] suburbGetter(String input){
-        if (input.equals("Helsinki"))
-            return getResources().getStringArray(R.array.helsinkiSuburbs);
-        else if (input.equals("Espoo"))
-            return getResources().getStringArray(R.array.espooSuburbs);
-        else
-            return getResources().getStringArray(R.array.vantaaSuburbs);
-
+    private String[] suburbGetter(int input) {
+        Resources res = getResources();
+        switch (input) {
+            case 0:
+                return res.getStringArray(R.array.helsinkiSuburbs);
+            case 1:
+                return res.getStringArray(R.array.vantaaSuburbs);
+            case 2:
+                return res.getStringArray(R.array.espooSuburbs);
+            default:
+                return new String[3];
+        }
     }
 
+    public void onClick(View view) {
+        // This code was pulled from https://stackoverflow.com/questions/34573201/change-languages-in-app-via-strings-xml
+        Locale svLocale = new Locale("sv");
+        Locale fiLocale = new Locale("fi");
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        if(view.getId() == R.id.tvFinnish)
+            conf.locale = fiLocale;
+        else if(view.getId() == R.id.tvSwedish)
+            conf.locale = svLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, ActivityHospitalSearch.class);
+        startActivity(refresh);
+    }
 }
--- app/src/main/java/com/example/hospitalguide/Hospitals.java
+++ app/src/main/java/com/example/hospitalguide/Hospitals.java
@@ -2,32 +2,33 @@ package com.example.hospitalguide;
 
 import java.util.ArrayList;
 
-public class Hospitals {
+class Hospitals {
     private ArrayList<Hospital> hospitalList = new ArrayList<>();
     private static Hospitals hospitals;
 
     private Hospitals(){
-        hospitalList.add(new Hospital("Helsinki Hospital", "Bulevardi 22, Helsinki", "Surgery", false));
+        hospitalList.add(new Hospital("Helsinki Hospital", "Bulevardi 22, Helsinki", "General", false));
         hospitalList.add(new Hospital("Mehiläinen Espoo Leppävaara", "Hevosenkenkä 3, Espoo", "General", false));
         hospitalList.add(new Hospital("Lastenlinna Children's Castle", "Lastenlinnantie 2, Helsinki", "Children's hospital", true));
     }
 
-    public static Hospitals getInstance(){
+    static Hospitals getInstance(){
         if(hospitals == null){
             hospitals = new Hospitals();
         }
         return hospitals;
     }
 
-    public ArrayList<String> getTreatments(){
+    ArrayList<String> getTreatments(){
         ArrayList<String> treatments = new ArrayList<>();
         for(Hospital hospital : this.hospitalList){
-            treatments.add(hospital.getTreatment());
+            if(!treatments.contains(hospital.getTreatment()))
+                treatments.add(hospital.getTreatment());
         }
         return treatments;
     }
 
-    public ArrayList<Hospital> filterHospitals(String location, String treatment){
+    ArrayList<Hospital> filterHospitals(String location, String treatment){
         ArrayList <Hospital> filteredList = new ArrayList<>();
         for(Hospital hospital : hospitalList){
             if(hospital.getLocation().contains(location) && hospital.getTreatment().contains(treatment))
@@ -36,7 +37,7 @@ public class Hospitals {
         return filteredList;
     }
 
-    public Hospital getHospital(String name){
+    Hospital getHospital(String name){
         for(Hospital hospital : hospitalList){
             if(hospital.getName().equals(name))
                 return hospital;
